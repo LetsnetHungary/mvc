03bc581816292679089cc9cecc1768f0760aff26
hegelakos <hegel.akos@letsnet.hu>
2017-04-14 21:39:48 +0200

    restore

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$site = $_GET["url"];
-
-$configjson = json_decode(file_get_contents("_resources/$site.json"));
-require("_views/_includes/_header.php");
-require "_views/$site/index.php";
-require("_views/_includes/_footer.php");
