20a1995caf31940364efffe92411d26c3d8186a7
hegelakos <hegel.akos@letsnet.hu>
2017-04-27 21:50:23 +0200

    mvc

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+require_once("vendor/autoload.php");
+
+$timezone = CoreApp\AppConfig::getTimeZone();
+date_default_timezone_set($timezone);
+
+//$analytics = new CoreApp\Controller\Analytics();
+
+//FRONTCONTROLLER
+
+$url = isset($_GET["url"]) ? $_GET["url"] : "Index";
+
+$app = new CoreApp\App($url);
+$app->build();
