ebd3cecbd242e823f1ae87afdb0643588cb26f09
adambalassa <balassa.adam@letsnet.hu>
2017-08-28 09:59:28 +0200

    better_routing

--- CoreApp/App.php
+++ CoreApp/App.php
@@ -5,79 +5,76 @@
         class App {
 
             private $routes;
+            private $uri;
             public function __construct() {
                 $uri = rtrim($_GET["url"], '/');
-                $uri = isset($_GET["url"]) ? explode("/", $uri) : [];
-                $this->getController($uri);
+                $this->uri = isset($_GET["url"]) ? explode("/", $uri) : [];
+                $this->getController();
             }
 
-            private function loop($uri, $routes) {
-              $return_array = array();
-                $c_uri = count($uri);
-                for($i = 0; $i <= $c_uri; $i++) {
-                    array_push($return_array, array());
-                    $c_routes = count($routes);
-                    for($k = 0; $k < $c_routes; $k++) {
-                        if($c_uri != count($routes[$k]) || ($routes[$k][$i] != $uri[$i] && strpos($routes[$k][$i], ":") != 1 )) {
-                            unset($routes[$k]);
-                        }
-                        else {
-                          array_push($return_array[$i], $k);
-                        }
+              private function getRouteMatches($routes){
+                foreach ($this->uri as $i => $urival) {
+                  foreach($routes as $k => $routeval){
+                    if(count($this->uri) != count($routeval) || ($urival != $routeval[$i] && strpos($routeval[$i], ":") != 1)){
+                      unset($routes[$k]);
                     }
-                    $routes = array_values($routes);
-                }
-                if(empty($routes)) {
-                  return "HTTPError";
-                }
-
-                $return_array["function"] = $this->prepareReturnArray($return_array);
-                for ($j=0; $j < $i - 1; $j++) {
-                  if (strpos($routes[0][$j], ":") == 1) {
-                    $return_array["parameters"][substr($routes[0][$j], 2, strlen($routes[0][$j]) - 3)] = $uri[$j];
                   }
                 }
-
-                return($return_array);
-            }
-
-            private function prepareReturnArray($array){
-              $c_a = count($array);
-              $index = 0;
-              for ($i = $c_a - 1; $i >= 0 ; $i--) {
-                $index = $array[$i][$index];
-              }
-              return $index;
-            }
-            private function getController($uri) {
-                $href = $uri[0];
-
-                $controllers = scandir("App/Controllers");
-                $return = false;
-                foreach($controllers as $controllerFile) {
-                    if($controllerFile ==  $href . ".php"){
-                        $return = $href;
-                    }
+                if(empty($routes)){
+                  return "HTTPError";
                 }
-                if (!$return) {
+                return $this->prepareReturnArray($routes);
+            }
+
+            private function prepareReturnArray($routes){
+              $return_array = array();
+              reset($routes);
+              $return_array["function"] = key($routes);
+              foreach($routes[$return_array["function"]] as $key => $value){
+                if (strpos($value, ":") == 1) {
+                  $return_array["parameters"][substr($value, 2, strlen($value) - 3)] = $this->uri[$key];
+                }
+              }
+              return $return_array;
+            }
+
+
+            private function getController() {
+                $href = $this->findController($this->uri);
+                if (!$href) {
                   require("App/Controllers/HTTPError.php");
                   return;
                 }
                 else {
                    require("App/Controllers/$href.php");
                 }
-                unset($uri[0]);
-                $uri = array_values($uri);
-                $this->routing($uri, $this->selectMethod($router));
+                unset($this->uri[0]);
+                $this->uri = array_values($this->uri);
+                $this->routing($this->selectMethod($router));
             }
-            private function routing($uri, $r)
+
+            private function findController($uri){
+              if($uri == null){
+                return "Index";
+              }
+              $href = $uri[0];
+              $controllers = scandir("App/Controllers");
+              $return = false;
+              foreach($controllers as $controllerFile) {
+                  if($controllerFile ==  $href . ".php"){
+                      $return = $href;
+                  }
+              }
+              return $return;
+            }
+            private function routing($r)
             {
               $routes = [];
               foreach($r as $route) {
                   array_push($routes, explode("/", $route));
               }
 
-              $route = $this->loop($uri, $routes);
+              $route = $this->getRouteMatches($routes);
               if ($route != "HTTPError") {
                 $this->routes[$route["function"]]["callback"]($route['parameters']);
               }
